d6d3a90e47acb41726198f142f78d1a21e8f689c
HoanBuCon <136064038+HoanBuCon@users.noreply.github.com>
2025-12-31 09:32:11 +0700

    Test Entrypoint

---
+++ backend/app/test_log.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, send_from_directory, jsonify, request
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.join(BASE_DIR, "..", "..", "frontend")
+
+app = Flask(__name__, static_folder=FRONTEND_DIR)
+
+@app.route("/api/mmb", methods=["GET", "POST"])
+def receive_text():
+    if request.method == "GET":
+        return jsonify({
+            "message": "Hello from /api/mmb (GET)"
+        })
+
+    if request.method == "POST":
+        data = request.get_json()
+        text = data.get("text")
+
+        print("📥 TEXT FROM FE:", text)
+
+        return jsonify({
+            "status": "ok",
+            "received": text
+        })
+
+if __name__ == "__main__":
+    app.run(port=5000)
